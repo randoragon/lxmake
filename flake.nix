9898a7533b71328bb83c3b8364da7481109ce6dc
randoragon <randoragongamedev@gmail.com>
2025-06-03 12:17:51 +0200

    build(flake.nix): Add missing dependencies and devshell

--- flake.nix
+++ flake.nix
@@ -8,31 +8,40 @@
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgsFor = nixpkgs.legacyPackages;
-      lxmake = pkgs: let
-        deps = with pkgs; [
-          graphviz
-          (texliveBasic.withPackages (ps: with ps; [
-            collection-latexrecommended
-            collection-fontsrecommended
-            collection-latexextra
-            collection-plaingeneric
-            collection-langcjk
-            collection-binextra
-            collection-bibtexextra
-          ]))
-        ];
-      in
-        pkgs.runCommand "lxmake" {
-          nativeBuildInputs = with pkgs; [ makeWrapper ];
-        } ''
-          mkdir -p "$out/bin"
-          cp ${./lxmake} "$out/bin/lxmake"
-          wrapProgram "$out/bin/lxmake" \
-            --prefix PATH : ${pkgs.lib.makeBinPath deps}
-        '';
+      deps = pkgs: with pkgs; [
+        coreutils
+        ncurses
+        libnotify mako
+        unixtools.getopt
+        gnugrep
+        graphviz
+        (texliveBasic.withPackages (ps: with ps; [
+          collection-latexrecommended
+          collection-fontsrecommended
+          collection-latexextra
+          collection-plaingeneric
+          collection-langcjk
+          collection-binextra
+          collection-bibtexextra
+        ]))
+      ];
+      lxmake = pkgs: pkgs.runCommand "lxmake" {
+        nativeBuildInputs = with pkgs; [ makeWrapper ];
+      } ''
+        mkdir -p "$out/bin"
+        cp ${./lxmake} "$out/bin/lxmake"
+        wrapProgram "$out/bin/lxmake" \
+          --prefix PATH : ${pkgs.lib.makeBinPath (deps pkgs)}
+      '';
     in {
       packages = forAllSystems (system: {
         default = lxmake pkgsFor.${system};
+      });
+
+      devShells = forAllSystems (system: {
+        default = pkgsFor.${system}.mkShell {
+          buildInputs = deps pkgsFor.${system};
+        };
       });
     };
 }
